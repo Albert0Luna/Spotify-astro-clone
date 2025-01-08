6d52ea7d3d6c2b84107e4fe2f5722fc2f2803114
Gustavo Alberto Robles Luna <111384757+Albert0Luna@users.noreply.github.com>
2025-01-08 18:42:36 +0000

    Improving and adding some features

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currentTime = new Date()
+  const currentHour = currentTime.getHours()
+
+  let greeting = ""
+
+  if (currentHour < 12) {
+    greeting = "Buenos días"
+  } else if (currentHour < 18) {
+    greeting = "Good morning"
+  } else {
+    greeting = "Good evening"
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
